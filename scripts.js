c1a1b792d9abaaf9740a2ee2a3b7bc67ce2ed72e
unknown <jasgarcia012@gmail.com>
2025-11-25 11:48:41 -0600

    website changes

---
+++ scripts.js
@@ -0,0 +1,39 @@
+//index.html
+
+const popin = document.querySelectorAll(".pop-in")
+
+setTimeout(() => {
+  popin.forEach(element => element.classList.add("show"));
+}, 300);
+
+const popup = document.querySelectorAll(".pop-up")
+
+setTimeout(() => {
+  popup.forEach(element => element.classList.add("show"));
+}, 200);
+
+
+
+//resume.html
+
+
+
+const url = 'Jason-Garcia resume.pdf';
+const canvas = document.getElementById("pdf-canvas");
+const context = canvas.getContext('2d')
+
+pdfjsLib.getDocument(url).promise.then(pdf => {
+  pdf.getPage(1).then(page => {
+    const scale = 1.3;
+    const viewport = page.getViewport({ scale });
+
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+
+    page.render({
+      canvasContext: context,
+      viewport: viewport
+    })
+  })
+})
+
